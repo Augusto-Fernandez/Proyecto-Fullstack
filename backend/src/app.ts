9ebf585c4c5246f445a58a6302064fc3d86a1810
Augusto Fernandez <augusfernandez23@gmail.com>
2023-10-23 18:35:13 -0300

    Se agregó validación y manejo de errores a los endpoints

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 
 import notesRouter from "./routes/notesRouter";
+import createHttpError, { isHttpError } from "http-errors"; // explica que cuando se import con {} es porque no es una importación default
 
 const app = express();
 
@@ -11,15 +12,20 @@ app.use(express.json());
 app.use("/api/notes", notesRouter);
 
 app.use((req, res, next) => {
-    next(Error("Endpoint not Found"));
+    next(createHttpError(404, "Endpoint not Found"));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((e: unknown, req: Request, res: Response, next: NextFunction) => { // e es unknown porque no sabe que recibe
     console.error(e);
         let errorMessage = "Hubo error che";
-        if (e instanceof Error) errorMessage = e.message;//explica que e siempre tiene la propiedad message
-        res.status(500).json({e:errorMessage});
+        let errorStatus = 500; // pasa 500 porque es el error standard
+        //if (e instanceof Error) errorMessage = e.message; explica que e siempre tiene la propiedad message
+        if(isHttpError(e)){
+            errorStatus = e.status;
+            errorMessage = e.message;
+        }
+        res.status(errorStatus).json({e:errorMessage});
 });
 
 export default app;
--- backend/src/controllers/notesController.ts
+++ backend/src/controllers/notesController.ts
@@ -1,5 +1,7 @@
 import { RequestHandler } from "express";
 import noteSchema from "../models/noteSchema";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
 export const getNotes: RequestHandler = async (req , res, next)=>{ // este tipado le da a req, res y next el tipado Request, Responce y NextFunction de Express
     try{
@@ -13,7 +15,15 @@ export const getNotes: RequestHandler = async (req , res, next)=>{ // este tipad
 export const getNote: RequestHandler = async (req, res, next) => {
     try {
         const noteId = req.params.id;
-        const note = await noteSchema.findById(noteId).exec();
+        const note = await noteSchema.findById(noteId).exec();// ver porque usa exec()
+
+        if(!mongoose.isValidObjectId(noteId)){
+            throw createHttpError(404, "Invalid Id");
+        }
+
+        if(!noteId){
+            throw createHttpError(404, "Note not Found");
+        }
 
         res.status(200).json(note);
     } catch (error) {
@@ -21,10 +31,20 @@ export const getNote: RequestHandler = async (req, res, next) => {
     }
 };
 
-export const createNote: RequestHandler = async (req, res, next) => {
+interface createNoteBody{
+    title?: string, // lo pone como opcional porque si no recibe title hace falta declinar la petición 
+    text?:string // lo pone como opcional porque en el esquema se declaró como opcional
+}
+
+export const createNote: RequestHandler<unknown, unknown, createNoteBody, unknown> = async (req, res, next) => { //no entendí porque lo pasó así, dice que va unknown porque any es inseguro
     try {
         const title = req.body.title;
         const text = req.body.text;
+
+        if(!title){
+            throw createHttpError(400, "Note must hace a title");
+        }
+
         const notes = await noteSchema.create({
             title: title,
             text: text
