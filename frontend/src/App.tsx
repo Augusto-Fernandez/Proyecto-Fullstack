774302eaa3c9c92c345340719ee2e09bf2ea4ab8
Augusto Fernandez <augusfernandez23@gmail.com>
2023-10-25 18:27:00 -0300

    Se creó el modelo de note

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,31 @@
-import React, {useState} from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+//import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
+//import { Button } from 'react-bootstrap';
+import { Note } from './models/note';
 
 function App() {
-  const [clickCount, setClickCount] = useState(0);
+  const [notes, setNotes] = useState<Note[]>([]); // se declara que va a recibir un array con elementos Note
+
+  useEffect(() => { // Hace que lo cambios ocurran cuando hace render
+    async function loadNotes (){ // Las notas se reciben por promesa pero useEffect no puede ser asíncrono, por eso se hace una función asincrona dentro
+      try {
+        const responce = await fetch("/api/notes", {method:"GET"});//hace el get a esa URL. Antes estaba http://localhost:8081/api/notes pero lo puso en el proxy del package.json porque había un problema de CORS
+        const notes = await responce.json(); //recibe el json de la respuesta
+        setNotes(notes); //hace que ocurra el cambio en el render
+      } catch (e) {
+        console.error(e);
+        alert(e);
+      }
+    }
+    loadNotes();
+  }, []) // al poner [], useEffect se ejecuta una sola vez al principio. Si no se pone, useEffect se ejecuta en cada render
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          El pedo 2011
-        </p>
-        <Button onClick={() => setClickCount(clickCount + 1)}>
-          Pedos: {clickCount}
-        </Button>
-      </header>
+      {JSON.stringify(notes)}
     </div>
   );
-}
+} //toma el json y lo convierte a string
 
 export default App;
---
+++ frontend/src/models/note.ts
@@ -0,0 +1,7 @@
+export interface Note {
+    _id: string,
+    title: string,
+    text?: string,
+    createdAt: string, // es el timestamp que se hizo en el esquema de mongo
+    updatedAt: string
+}
